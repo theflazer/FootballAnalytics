6b6e0bf863cd56ff4913c93f16628960f26be9ab
Flazer <the.flazer@gmail.com>
2014-11-21 22:02:45 +0800

    Project Initiation

---
+++ TrueFootballAnalytics/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Flazer on 11/21/2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Guys. Sample Maven Project Here");
+    }
+}
